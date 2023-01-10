5ebf98e36b021661c60b9fb7e808f4295fd31a2e
jisoo jamie <jisoojamie@gmail.com>
2023-01-10 11:37:44 +0900

    todo-list by JQuery

---
+++ 230110/case-jquery/index.js
@@ -0,0 +1,26 @@
+$(function () {
+
+    $("#create").on("submit", function (event) {
+        event.preventDefault();
+        var value = $(this).find("input").val();
+
+        $("#todo-list").append(
+            '<li>'+
+                '<span>' + 
+                value + 
+                '</span>'+
+                '<button type="button" class="complete">완료</button> '+
+                '<button type="button" class="remove">삭제</button>'+
+            '</li>'
+        );
+
+        $(this).trigger("reset");
+    });
+
+    $("body").on("click", ".complete", function () {
+        $(this).parent("li").addClass("completed")
+    });
+    $("body").on("click", ".remove", function () {
+        $(this).parent("li").remove();
+    });
+});
